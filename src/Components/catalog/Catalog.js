41b4eecb4ba835951e0560a43831de3d3f1cec8a
Kyle Hoffman <hoffmankylejnr@gmail.com>
2019-04-25 08:05:15 +0200

    Progress on catalog made, still not done

--- src/Components/catalog/Catalog.js
+++ src/Components/catalog/Catalog.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
 
-export default class Catalog extends Component {
+import Spinner from '../layout/Spinner'
+
+class Catalog extends Component {
 	render() {
-		return (
-			<div>
-				<h1>Catalog</h1>
-			</div>
-		)
+		const { catalog } = this.props
+		console.log('CATALOG', catalog)
+		if (catalog) {
+			return (
+				<div>
+					<h4 className="text-center">This is the books things</h4>
+					<ul>
+						{catalog.map(item => (
+							<li className="list-group-item" key={item.uid}>
+								<div className="row">
+									<div className="col-4">
+										<h5 className="mb-0">{item.bookTitle}</h5>
+									</div>
+									<div className="col-4">{item.author}</div>
+								</div>
+								<div className="row">
+									<div className="col-4">{item.subTitle}</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			)
+		} else {
+			return <Spinner />
+		}
 	}
 }
+
+export default compose(
+	firestoreConnect([{ collection: 'catalog' }]),
+	connect((state, props) => ({ catalog: state.firestore.ordered.catalog }))
+)(Catalog)
